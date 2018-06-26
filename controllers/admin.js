b3350ad7abbc3d5172570123c55956753a8c02b7
Quyền <HoangTheQuyen01@gmail.com>
2018-06-26 21:00:00 +0700

    Quản lý sản phẩm

--- controllers/admin.js
+++ controllers/admin.js
@@ -20,7 +20,38 @@ function index(req, res, next){
 	});
 
 }
+function insertproduct(req, res, next){
+	var datetime = new Date();
+	var product = {
+		name : req.body.product_name,
+		category_id: req.body.category_id,
+		images: req.body.images,
+		description: req.body.description,
+		origin: req.body.origin,
+		inventory_number: req.body.inventory_number,
+		price: req.body.price,
+		category: req.body.category,
+		datetime: datetime.getFullYear() + "-" + datetime.getMonth() + "-" + datetime.getDate(),
+		vendor_id: req.body.vendor_id
+	}
+	productRepo.insert(product).then(function(results){
+		res.status(200).json({err: false});
+	}).catch(function(err){
+		next(err);
+	});
+}
+function deleteProductById(req, res, next){
+	var productname = req.params.product_id;
+	productRepo.deleteProductById(productname).then(function(results){
+		res.json({status: 200});
+	}).catch(function(err){
+		next(err);
+	});
+}
+
 
 module.exports =  {
-	"index": index
+	"index": index,
+	"insertproduct": insertproduct,
+	"deleteProductById": deleteProductById
 };
