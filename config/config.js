346a855b071d4f5516ea3fb56d5c84fc0009bb00
Quyền <HoangTheQuyen01@gmail.com>
2018-06-29 12:51:05 +0700

    Complete admin

--- config/config.js
+++ config/config.js
@@ -1,10 +1,9 @@
 module.exports = {
 	db: {
-	host: 'localhost',
+	host: '127.0.0.1',
         port: 3306,
         user: 'root',
-        password: '1234',
-        database: 'mydb',
-        timezone: 'utc'
+        password: 'Quyen1996',
+        database: 'mydb'
 	}
 }
